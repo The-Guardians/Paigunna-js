be10924fbee59c810c7e908548bb78da5f8802cd
Student554 <awirut091@gmail.com>
2018-04-07 15:58:56 +0700

    test run popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+document.getElementById('popuplogin').style.display='block';
+
+function login(){
+    document.getElementById('popuplogin').style.display='none';
+}
+function logout(){
+    document.getElementById('popuplogin').style.display='block';  
+}
 function payment(){
     document.getElementById('paymentForm').style.display = 'block';
 }
