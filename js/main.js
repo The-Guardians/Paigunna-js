f0cc4175d350bb23db73e03797756a135237f108
Student554 <awirut091@gmail.com>
2018-04-21 18:14:27 +0700

    add button toggle service

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.getElementById('popuplogin').style.display = 'block';
-document.getElementById('signOut').style.display = 'none';
+document.getElementById('signout').style.display = 'none';
+document.getElementById('signout1').style.display = 'none';
 document.getElementById('name').style.display = 'none';
 document.getElementById('tel').style.display = 'none';
 document.getElementById('email').style.display = 'none';
@@ -7,7 +8,8 @@ document.getElementById('callService').disabled=true;
 
 function login() {
     document.getElementById('popuplogin').style.display = 'none';
-    document.getElementById('signOut').style.display = 'block';
+    document.getElementById('signout').style.display = 'block';
+    document.getElementById('signout1').style.display = 'block';
     document.getElementById('name').style.display = 'block';
     document.getElementById('tel').style.display = 'block';
     document.getElementById('email').style.display = 'block';
@@ -17,11 +19,6 @@ function callService() {
     document.getElementById('callService').disabled=false;
 }
 
-function myFunction() {
-    var x = document.getElementById("mySelect").selectedIndex;
-    var y = document.getElementById("mySelect").options;
-    alert("Index: " + y[x].index + " is " + y[x].text);
-}
 /*function logout() {
     document.getElementById('popuplogin').style.display = 'block';
     document.getElementById('signOut').style.display = 'none';
