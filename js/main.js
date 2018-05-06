80ba557b346eee4e6b187dc8d2f57f06987bb622
Student554 <awirut091@gmail.com>
2018-05-06 14:31:20 +0700

    edit size pop up

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.getElementById('tel').style.display = 'none';
 document.getElementById('email').style.display = 'none';
 document.getElementById('callService').disabled=true;
 document.getElementById('price').style.display = 'none';
+document.getElementById('itemPay').style.display = 'none';
 
 function login() {
     document.getElementById('popuplogin').style.display = 'none';
@@ -19,8 +20,8 @@ function login() {
 function callService() {
     document.getElementById('callService').disabled=false;
     document.getElementById('price').style.display = 'block';
+    document.getElementById('itemPay').style.display = 'block';
 }
-
 
 /*function logout() {
     document.getElementById('popuplogin').style.display = 'block';
