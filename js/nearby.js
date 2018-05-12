5bde0ffdad75cf7cd20ce48c2c47e21be4656684
MikuMiku <34748866+Wasin007@users.noreply.github.com>
2018-05-12 17:05:42 +0700

    wasin pull

--- js/nearby.js
+++ js/nearby.js
@@ -1,14 +1,15 @@
 import { findCurrentLocation } from './findmylocate';
 import { initMap } from './mainmap';
 
-export function nearbyRes() {
+export function nearbyTour() {
     let map = initMap();
     var pyrmont = findCurrentLocation(map);
 
     var request = {
         location: pyrmont,
-        radius: '500',
-        type: ['restaurant']
+        radius: '2000',
+        type: ['zoo','shopping_mall','museum','movie_theater','aquarium','amusement_park','night_club'
+    ,'park']
     };
 
     service = new google.maps.places.PlacesService(map);
